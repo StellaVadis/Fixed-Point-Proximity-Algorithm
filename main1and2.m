e2093c94e2e141efe5f88d0d06e9914285ade15e
StellaVadis <99245232+StellaVadis@users.noreply.github.com>
2022-11-09 18:16:59 +0800

    Delete main1and2.m

--- main1and2.m
+++
@@ -1,9 +0,0 @@
-Images=imread('Cameraman.png');
-imshow(Images,[])
-figure(2)
-NoiseLevel=20;
-sigma=(NoiseLevel*NoiseLevel)/(255*255);
-Im_noisy=imnoise(Images,'gaussian',0,sigma);
-%Save the noisy image
-imwrite(uint8(Im_noisy),'Noisy Image20.png')
-imshow(im2double(Im_noisy),[]);
